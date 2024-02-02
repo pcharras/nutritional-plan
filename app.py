bd83c000687d0db693f8aa3c52d04749e43f1779
JaviPeder~ <javierpedernera@gmail.com>
2024-02-02 10:51:16 -0300

    add origin url

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ url3 = os.getenv('URL3')
 
 app = Flask(__name__)
 
-cors = CORS(app, resources={r"*": {"origins": "https://app-nutri-plan-ia.vercel.app"}})
+cors = CORS(app, resources={r"*": {"origins": "https://app-main-nutri-plan-ia.vercel.app"}})
 
 # Configuración de logueo
 logging.basicConfig(level=logging.INFO)
